c21e3617c9286cdc88ed0ef1cb2f9b5b72932f4e
belladwir <belladwirh@gmail.com>
2021-11-09 12:45:16 +0700

    Initial Commit

---
+++ MySimpleHeroApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleHeroApp
+//
+//  Created by Bella Dwi Rahmatulia on 09/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
